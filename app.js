cad228f9476bd6b51dd252504ff1622432ef3134
Andreas Ekström <ekstrom.andreas@gmail.com>
2013-06-12 07:27:17 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,40 @@
+
+/**
+ * Module dependencies.
+ */
+
+var viewer = require('./routes/viewer');
+
+var express = require('express')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+app.get('/', viewer.home);
+// display the list of item
+app.get('/features', viewer.features);
+// show individual feature
+app.get('/feature/:id', viewer.feature);
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(require('stylus').middleware(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ routes/viewer.js
@@ -0,0 +1,51 @@
+function list_features(callback) {
+  var execFile = require('child_process').execFile;
+  var walk    = require('walk');
+  var files = [];
+
+  var walker  = walk.walk(process.env.FEATURES_HOME, { followLinks: false });
+
+  walker.on('file', function(root, stat, next) {
+    filename = stat.name;
+    matches =  filename.match(/(.*)\.(.*)$/);
+    if(matches[2] == 'feature')
+      files.push(matches[1]);
+    next();
+  });
+
+  walker.on('end', function() {
+    callback(files);
+  });
+}
+
+function read_file(path) {
+  console.log("In function: " + path);
+  var fs = require('fs');
+  return fs.readFileSync(path, 'utf8');
+}
+
+
+exports.home = function(req, res){
+  res.render('home', { title: 'Cucumber viewer' })
+};
+
+// handler for displaying the features
+exports.features = function(req, res) {
+  callback = function(files) {
+    res.render('features', { title: 'Documentation - features', features:files });
+  }
+
+  list_features(callback);
+};
+
+// handler for displaying individual features
+exports.feature = function(req, res) {
+  var name = req.params.id;
+  //feature = "Feature: Passive users\n Scenario: Creating a new passive user"
+  var filename = process.env.FEATURES_HOME + "/" + name + ".feature";
+  var feature = read_file(filename);
+  feature = feature.replace("'","´");
+
+  res.render('feature', { title: 'Feature - ' + name, feature: feature });
+};
+
